da4f09f9d3f8bda2bf175cf81fc342b6908410e3
David Sun <iamslash@gmail.com>
2016-03-02 11:07:47 +0900

    refactored install, uninstall

--- install-all.sh
+++ install-all.sh
@@ -15,7 +15,7 @@ die() {
 [ -e $EMACS_ROBIN_HOME ] && die "$EMACS_ROBIN_HOME already exists."
 [ -e "~/.emacs" ] && die "~/.emacs already exists."
 
-# clone vimrc
+# clone emacs_robin
 git clone git://github.com/iamslash/emacs_robin.git $EMACS_ROBIN_HOME || die "git clone failed."
 ln -s "$EMACS_ROBIN_HOME/.emacs" ~/.emacs || die ".emacs symbolic link failed."
 
--- uninstall-all.sh
+++ uninstall-all.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-VIM_ROBIN_HOME=~/.emacs_robin
+EMACS_ROBIN_HOME=~/.emacs_robin
 
-rm -rf $VIM_ROBIN_HOME
+rm -rf $EMACS_ROBIN_HOME
 rm ~/.emacs
 
 echo "emacs_robin is uninstalled."
