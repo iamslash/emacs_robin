4f477518fef2a1ca7126668a592e399dcc486e7f
iamslash <iamslash@gmail.com>
2016-10-19 15:33:52 +0900

    added autoinsert for cs, go

---
+++ autoinsert/autoinsert.cs
@@ -0,0 +1,23 @@
+// Copyright (C) 2016 by iamslash
+
+using System;
+
+namespace Main
+{
+    /// <summary>
+    /// MainApp startup class for Real-World
+    /// </summary>
+    class MainApp
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main()
+        {
+            Console.WriteLine("Hello World\n");
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
