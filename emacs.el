83b0e739000e59f9490c09d37ed62b8bfa3e0a08
iamslash <iamslash@gmail.com>
2019-09-29 00:50:18 -0700

    update golang tab width

--- emacs.el
+++ emacs.el
@@ -305,6 +305,7 @@
   (local-set-key (kbd "M-.") 'godef-jump)
   (setq tab-width 2)
   (setq standard-indent 2)
+  (setq default-tab-width 2)
   (setq indent-tabs-mode nil))
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 
